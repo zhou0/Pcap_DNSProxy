9ee2b880edd5f906833d3a8a2551b837364349bf
zhou0 <5291860+zhou0@users.noreply.github.com>
2026-04-18 00:33:38 +0000

    build: update CMake build system to 3.5+ and modernize targets

---
+++ build/Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/app/Source/Pcap_DNSProxy/Base.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Base.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Base.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/Capture.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Capture.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Capture.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/Configuration.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Configuration.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Configuration.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/DNSCurveControl.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/DNSCurveControl.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/DNSCurveControl.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/DNSCurveRequest.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/DNSCurveRequest.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/DNSCurveRequest.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/Initialization.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Initialization.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Initialization.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/Main.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Main.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Main.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/Monitor.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Monitor.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Monitor.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/Network.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Network.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Network.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/PacketData.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/PacketData.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/PacketData.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/PrintLog.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/PrintLog.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/PrintLog.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/Process.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Process.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Process.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/Protocol.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Protocol.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Protocol.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/Proxy.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Proxy.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Proxy.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/ReadCommand.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/ReadCommand.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/ReadCommand.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/ReadHosts.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/ReadHosts.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/ReadHosts.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/ReadIPFilter.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/ReadIPFilter.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/ReadIPFilter.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/ReadParameter.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/ReadParameter.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/ReadParameter.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/Request.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Request.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Request.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/Service.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Service.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/Service.cpp.o.d"
+  "/app/Source/Pcap_DNSProxy/TransportSecurity.cpp" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/TransportSecurity.cpp.o" "gcc" "Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/TransportSecurity.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Source/Pcap_DNSProxy/CMakeFiles/Pcap_DNSProxy.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Pcap_DNSProxy.
+# This may be replaced when dependencies are built.
